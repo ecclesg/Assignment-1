2be7e5c108cf881b0c50bbe263df77066f1964d1
Gavin Eccles <ecclesg@tcd.ie>
2017-10-12 15:49:39 +0100

    New Committ

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -1,75 +1,70 @@
-class Node
-{
+import java.util.ArrayList;
+import java.util.List;
+ 
+class Node {
     int data;
     Node left, right;
  
-    Node(int item)
-    {
-        data = item;
+    Node(int value) {
+        data = value;
         left = right = null;
     }
 }
  
-public class LowestCommonAncestor
+public class LowestCommonAncestor 
 {
-    //Root of the Binary Tree
+ 
     Node root;
-    static boolean found1 = false, found2 = false;
-    
+    private List<Integer> path1 = new ArrayList<>();
+    private List<Integer> path2 = new ArrayList<>();
  
-    Node findLCA(int n1, int n2)
-    {
-    	found1 = false;
-        found2 = false;
- 
-        Node lca = findLCA(root, n1, n2);
- 
-        // Return LCA only if both n1 and n2 are present in tree
-        if (found1 && found2)
-            return lca;
- 
-        // Else return NULL
-        return null;
+    int findLCA(int n1, int n2) {
+        path1.clear();
+        path2.clear();
+        return findLCAInternal(root, n1, n2);
     }
  
-    // This function returns pointer to LCA of two given
-    // values n1 and n2.
-    // found1 is set as true by this function if n1 is found
-    // found2 is set as true by this function if n2 is found
-    private Node findLCA(Node node, int n1, int n2)
-    {
-    	// Base case
-        if (node == null)
-            return null;
+    private int findLCAInternal(Node root, int n1, int n2) {
  
-        // If either n1 or n2 matches with root's key, report the presence
-        // by setting found1 or found2 as true and return root (Note that if a key
-        // is ancestor of other, then the ancestor key becomes LCA)
-        if (node.data == n1)
-        {
-        	found1 = true;
-            return node;
-        }
-        if (node.data == n2)
-        {
-        	found2 = true;
-            return node;
+        if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) {
+            return -1;
         }
  
-        // Look for keys in left and right subtrees
-        Node left_lca = findLCA(node.left, n1, n2);
-        Node right_lca = findLCA(node.right, n1, n2);
+        int i;
+        for (i = 0; i < path1.size() && i < path2.size(); i++) {
+          //  System.out.println(path1.get(i) + " " + path2.get(i));
+            if (!path1.get(i).equals(path2.get(i)))
+                break;
+        }
  
-        // If both of the above calls return Non-NULL, then one key
-        // is present in once subtree and other is present in other,
-        // So this node is the LCA
-        if (left_lca != null && right_lca != null)
-            return node;
- 
-        // Otherwise check if left subtree or right subtree is LCA
-        return (left_lca != null) ? left_lca : right_lca;
+        return path1.get(i-1);
     }
  
-}
+    private boolean findPath(Node root, int n, List<Integer> path)
+    {
+        if (root == null) {
+            return false;
+        }
+ 
+        path.add(root.data);
+ 
+        if (root.data == n) {
+            return true;
+        }
+ 
+        if (root.left != null && findPath(root.left, n, path)) {
+            return true;
+        }
+ 
+        if (root.right != null && findPath(root.right, n, path)) {
+            return true;
+        }
+ 
+        path.remove(path.size()-1);
+ 
+        return false;
+    }
+ 
 
-//Class sourced from http://www.geeksforgeeks.org/lowest-common-ancestor-binary-tree-set-1/
+}
+// This code is contributed by Sreenivasulu Rayanki.
--- src/LowestCommonAncestorTEST.java
+++ src/LowestCommonAncestorTEST.java
@@ -1,3 +1,4 @@
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,13 +8,13 @@ public class LowestCommonAncestorTEST {
 	
 	@Test
 	public void testForEmptyTree(){
-		LowestCommonAncestor treeEmpty = new LowestCommonAncestor();
-		assertNull("LCA(Empty)",treeEmpty.findLCA(0, 0));
+		LowestCommonAncestor2 treeEmpty = new LowestCommonAncestor2();
+		assertEquals("LCA(Empty)",-1,treeEmpty.findLCA(0, 0));
 	}
 	
 	@Test
 	public void testForTreeSize7(){
-		LowestCommonAncestor treeSize7 = new LowestCommonAncestor();
+		LowestCommonAncestor2 treeSize7 = new LowestCommonAncestor2();
 		treeSize7.root = new Node(1);
 		treeSize7.root.left = new Node(2);
 		treeSize7.root.right = new Node(3);
@@ -22,16 +23,16 @@ public class LowestCommonAncestorTEST {
 		treeSize7.root.right.left = new Node(6);
 		treeSize7.root.right.right = new Node(7);
         
-        assertEquals("LCA(4,5)", 2,treeSize7.findLCA(4, 5).data);
-        assertEquals("LCA(4,6)", 1,treeSize7.findLCA(4, 6).data);
-        assertEquals("LCA(3,4)", 1,treeSize7.findLCA(3, 4).data);
-        assertEquals("LCA(6,7)", 3,treeSize7.findLCA(6, 7).data);
-        assertEquals("LCA(5,6)", 1,treeSize7.findLCA(5, 6).data);
+        assertEquals("LCA(4,5)", 2,treeSize7.findLCA(4, 5));
+        assertEquals("LCA(4,6)", 1,treeSize7.findLCA(4, 6));
+        assertEquals("LCA(3,4)", 1,treeSize7.findLCA(3, 4));
+        assertEquals("LCA(6,7)", 3,treeSize7.findLCA(6, 7));
+        assertEquals("LCA(5,6)", 1,treeSize7.findLCA(5, 6));
 	}
 	
 	@Test
 	public void testForTreeSize15(){
-		LowestCommonAncestor treeSize15 = new LowestCommonAncestor();
+		LowestCommonAncestor2 treeSize15 = new LowestCommonAncestor2();
 		treeSize15.root = new Node(1);
 		treeSize15.root.left = new Node(2);
 		treeSize15.root.right = new Node(3);
@@ -48,26 +49,24 @@ public class LowestCommonAncestorTEST {
 		treeSize15.root.right.right.left = new Node(14);
 		treeSize15.root.right.right.right = new Node(15);
 		
-		assertEquals("LCA(14,15)", 7,treeSize15.findLCA(14, 15).data);
-        assertEquals("LCA(8,15)", 1,treeSize15.findLCA(8, 15).data);
-        assertEquals("LCA(8,9)", 4,treeSize15.findLCA(8, 9).data);
-        assertEquals("LCA(9,13)", 1,treeSize15.findLCA(9, 13).data);
-        assertEquals("LCA(10,11)", 5,treeSize15.findLCA(10, 11).data);
+		assertEquals("LCA(14,15)", 7,treeSize15.findLCA(14, 15));
+        assertEquals("LCA(8,15)", 1,treeSize15.findLCA(8, 15));
+        assertEquals("LCA(8,9)", 4,treeSize15.findLCA(8, 9));
+        assertEquals("LCA(9,13)", 1,treeSize15.findLCA(9, 13));
+        assertEquals("LCA(10,11)", 5,treeSize15.findLCA(10, 11));
         
 	}
 	
 	@Test
 	public void testForTreeSize1(){
-		LowestCommonAncestor treeSize1 = new LowestCommonAncestor();
+		LowestCommonAncestor2 treeSize1 = new LowestCommonAncestor2();
 		treeSize1.root = new Node(1);
-		assertNull("LCA(1,0)",treeSize1.findLCA(1, 0));
-	
-	
+		assertEquals("LCA(1,0)",-1,treeSize1.findLCA(1, 0));
 	}
 
 	@Test
 	public void testForNonExitingNodes(){
-		LowestCommonAncestor treeNonExistentNode = new LowestCommonAncestor();
+		LowestCommonAncestor2 treeNonExistentNode = new LowestCommonAncestor2();
 		treeNonExistentNode.root = new Node(4);
 		treeNonExistentNode.root.left = new Node(8);
 		treeNonExistentNode.root.right = new Node(9);
@@ -76,19 +75,19 @@ public class LowestCommonAncestorTEST {
 		treeNonExistentNode.root.right.left = new Node(17);
         treeNonExistentNode.root.right.right = new Node(21);
         
-        assertEquals("LCA(12,13)",8,treeNonExistentNode.findLCA(12, 13).data);
-        assertEquals("LCA(17,21)",9,treeNonExistentNode.findLCA(17, 21).data);
-        assertEquals("LCA(13,21)",4,treeNonExistentNode.findLCA(13, 21).data);
-        
-        assertNull("LCA(13,44)",treeNonExistentNode.findLCA(13, 44));
-        assertNull("LCA(6,18)",treeNonExistentNode.findLCA(6, 18));
-        assertNull("LCA(11,23)",treeNonExistentNode.findLCA(11, 23));
+        assertEquals("LCA(12,13)",8,treeNonExistentNode.findLCA(12, 13));
+        assertEquals("LCA(17,21)",9,treeNonExistentNode.findLCA(17, 21));
+        assertEquals("LCA(13,21)",4,treeNonExistentNode.findLCA(13, 21));
+                
+        assertEquals("LCA(13,44)",-1,treeNonExistentNode.findLCA(13, 44));
+        assertEquals("LCA(6,18)",-1,treeNonExistentNode.findLCA(6, 18));
+        assertEquals("LCA(11,23)",-1,treeNonExistentNode.findLCA(11, 23));
 
 	}
 	
 	@Test
 	public void testWhenOneNodeIsAncestorOfOther(){
-		LowestCommonAncestor treeAncestorNode = new LowestCommonAncestor();
+		LowestCommonAncestor2 treeAncestorNode = new LowestCommonAncestor2();
 		treeAncestorNode.root = new Node(11);
 		treeAncestorNode.root.left = new Node(21);
 		treeAncestorNode.root.right = new Node(5);
@@ -97,12 +96,12 @@ public class LowestCommonAncestorTEST {
 		treeAncestorNode.root.right.left = new Node(9);
         treeAncestorNode.root.right.right = new Node(33);
 		
-        assertEquals("LCA(7,8)",21,treeAncestorNode.findLCA(7, 8).data);
+        assertEquals("LCA(7,8)",21,treeAncestorNode.findLCA(7, 8));
          
         //Test when one is the ancestor
-        assertEquals("LCA(7,21)", 21,treeAncestorNode.findLCA(7, 21).data);
-        assertEquals("LCA(5,9)", 5,treeAncestorNode.findLCA(5, 9).data);
-        assertEquals("LCA(11,21)", 11,treeAncestorNode.findLCA(11, 21).data);
+        assertEquals("LCA(7,21)", 21,treeAncestorNode.findLCA(7, 21));
+        assertEquals("LCA(5,9)", 5,treeAncestorNode.findLCA(5, 9));
+        assertEquals("LCA(11,21)", 11,treeAncestorNode.findLCA(11, 21));
         
         
 
@@ -110,7 +109,7 @@ public class LowestCommonAncestorTEST {
 	
 	@Test
 	public void testForALlSameNode(){
-		LowestCommonAncestor sameNode = new LowestCommonAncestor();
+		LowestCommonAncestor2 sameNode = new LowestCommonAncestor2();
 		sameNode.root = new Node(3);
 		sameNode.root.left = new Node(3);
 		sameNode.root.right = new Node(3);
@@ -119,7 +118,7 @@ public class LowestCommonAncestorTEST {
 		sameNode.root.right.left = new Node(3);
 		sameNode.root.right.right = new Node(3);
 		
-		assertEquals("LCA(3,3)",3,sameNode.findLCA(3, 3).data);
+		assertEquals("LCA(3,3)",3,sameNode.findLCA(3, 3));
 		
 	}
 	
