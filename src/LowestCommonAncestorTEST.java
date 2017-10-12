15d575a8769bfbece77908447cc924dbd58935fd
Gavin Eccles <ecclesg@tcd.ie>
2017-10-12 10:49:44 +0100

    Fix Test Failure

---
+++ src/LowestCommonAncestorTEST.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LowestCommonAncestorTEST {
+	LowestCommonAncestor tree = new LowestCommonAncestor();
+	
+	@Test
+	public void testFindLCA(){
+		
+	
+		
+		
+		
+	}
+	
+	@Test
+	public void testFindLCAForTree15(){
+		LowestCommonAncestor tree = new LowestCommonAncestor();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+        
+        assertEquals("LCA(4,5)", 2,tree.findLCA(4, 5).data);
+        assertEquals("LCA(4,6)", 1,tree.findLCA(4, 6).data);
+        assertEquals("LCA(3,4)", 1,tree.findLCA(3, 4).data);
+        assertEquals("LCA(2,4)", 2,tree.findLCA(2, 4).data);
+        assertEquals("LCA(6,7)", 3,tree.findLCA(6, 7).data);
+        assertEquals("LCA(5,6)", 1,tree.findLCA(5, 6).data);
+	}
+	
+	
+	
+	
+	
+	
+}
